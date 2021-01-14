ec26ffdf8e78887a9818b5a7a3d60b58f688d308
CouchK <karencouch97@gmail.com>
2021-01-13 23:40:28 -0500

    Changed positioning inside modal

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function display()
+{
+    document.querySelector('.proj-modal').style.display = 'flex';
+}
+
+function hide()
+{
+    document.querySelector('.proj-modal').style.display = 'none';
+}
