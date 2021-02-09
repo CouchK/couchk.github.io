023388234d2aea33033958ee558f4213fa9f3f09
CouchK <karencouch97@gmail.com>
2021-02-08 21:59:45 -0500

    Added carousel for project images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-function display()
-{
-    document.querySelector('.proj-modal').style.display = 'flex';
-}
 
-function hide()
-{
-    document.querySelector('.proj-modal').style.display = 'none';
-}
+
+
